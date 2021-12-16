fcb22ff3509a43b0cd767570c861df19e0ebaffb
Halil <halil000ibrahim@gmail.com>
2021-12-16 19:50:15 +0300

    5: - implement klotski - add klotski tests to main

--- board_game2d.cpp
+++ board_game2d.cpp
@@ -1,7 +1,7 @@
 #include "board_game2d.h"
 
 namespace BoardGame {
-    void playVector(std::vector<BoardGame2D *> games) {
+    void BoardGame2D::playVector(std::vector<BoardGame2D *> games) {
         for (size_t i = 0; i < games.size(); i++) {
             games[i]->playAutoAll();
         }
@@ -12,7 +12,7 @@ namespace BoardGame {
         std::string input;
 
         do {
-            std::cout << "Enter your command(c2-u, g6-l, b1-d, c3-l): ";
+            std::cout << "Enter your command(c2-u, g6-l, b1-d, c3-l or tile number 0-r, 1-d, 7-u): ";
             getline(std::cin, input);
             success = playUser(input);
         } while (success == 0);
@@ -20,13 +20,18 @@ namespace BoardGame {
     }
 
     void BoardGame2D::playAutoAll() {
-        std::cout << "AUTO ALL" << std::endl;
+        int counter = 0;
+
+        // std::cout << "AUTO ALL" << std::endl;
         for (int i = 0; i < 15; i++)
             std::cout << std::endl;
         while (!endGame()) {
             playAuto();
             print("", true);
+            counter++;
         }
+        std::cout << "Solved in " << counter << " moves." << std::endl;
+        std::cout << "Board score: " << boardScore() << std::endl;
     }
 
     std::ostream &operator<<(std::ostream &outs, const BoardGame2D &game) {
